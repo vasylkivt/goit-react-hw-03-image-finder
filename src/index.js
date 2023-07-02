13982cfde3e2bb64128035e4ebea74d7e6c8bcdf
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-07-02 20:41:11 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App/App';
 import './index.css';
 
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'constants/theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
